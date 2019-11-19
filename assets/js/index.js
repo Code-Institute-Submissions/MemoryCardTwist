619830d9e715a09a186874edb4c139c082c13eec
Stephen Jackson <jjackson19862017@gmail.com>
2019-11-19 13:22:13 +0000

    Hide/Show guess function

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,7 @@ var generatedGuessString = ""; //Empty string
 
 //Function that is run on index.html load up
 function init() {
+    debugger;
     //Card values when they are flipped
     cardArray = ['0001', '0001', '0010', '0010', '0011', '0011', '0100', '0100', '0101', '0101', '0110', '0110', '0111', '0111', '1000', '1000', '1001', '1001', '1010', '1010', '1011', '1011', '1100', '1100']; //Values that will appear when the card is clicked on.
     //Default leaderboard values
@@ -36,7 +37,7 @@ function init() {
         console.log(sortedArray);
     });
     generateLeaderboard();
-    document.getElementById("score_area").innerHTML = updateLeaderboard;
+    hideGuess();
     //Creates the shuffle process for the array
     Array.prototype.cardShuffle = function() {
         var n = this.length,
@@ -49,6 +50,144 @@ function init() {
         }
     };
 };
+
+
+
+function newGame() {
+
+    cardFlipped = 0; //Reset Value
+    turnsTaken = 0; //Reset Value
+    guessesLeft = 3; //Reset Value
+    //Calls the shuffle command and adds Divs to the game_area
+    var out = '';
+    cardArray.cardShuffle();
+    for (var i = 0; i < cardArray.length; i++) {
+        out += '<div id="card_' + i + '" onclick="cardFlip(this,\'' + cardArray[i] + '\')"></div>';
+    }
+    out += '<span>' + guessingString(10) + '</span>';
+    turnsTakenString = ""; //Hides the text so that it doesnt look unsightly
+    document.getElementById("turnCounter").innerHTML = turnsTakenString;
+    document.getElementById('game_area').innerHTML = out;
+    document.getElementById("guessButton").innerText = "3 Guesses Left";
+    showGuess();
+    console.log(generatedGuessString);
+};
+
+function cardFlip(card, val) {
+    //Flipping Cards
+    if (card.innerHTML == "" && cardValues.length < 2) { //If the cards match
+        card.style.background = '#FFF';
+        card.style.backgroundImage = 'radial-gradient(lightblue, cornflowerblue)'; //Displays a circle of light blue, behind the text
+        card.innerHTML = val;
+        if (cardValues.length == 0) {
+            turnCounting();
+            cardValues.push(val); //Push the Value into the array
+            cardIds.push(card.id); //Push the Card Id into the array
+        } else if (cardValues.length == 1) {
+            cardValues.push(val); //Clicking on the second card and pushing the Value into the Array
+            cardIds.push(card.id); //Pushing the second Card ID
+            if (cardValues[0] == cardValues[1]) {
+                cardFlipped += 2;
+                var card1 = document.getElementById(cardIds[0]);
+                var card2 = document.getElementById(cardIds[1]);
+                card1.style.visibility = 'hidden'; //Once match is made hide the cards
+                card2.style.visibility = 'hidden'; //Once match is made hide the cards
+                cardValues = []; // Clear array
+                cardIds = []; // Clear array
+                // Check to see if the whole board is cleared
+                if (cardFlipped == cardArray.length) { //If all the cards have been flipped
+                    //alert("Game Completed, you have taken " + turnsTaken + " turns to complete the game.  Lets see where you have come on the Leader Board");
+                    document.getElementById('game_area').innerHTML = "Well Done";
+                    playerNameInput(); // Take players name
+                    results(); //Leaderboard functions
+                }
+            } else { //This will run if there are no matches
+                function flipOver() {
+                    // Flip the 2 tiles back over
+                    var card1 = document.getElementById(cardIds[0]);
+                    var card2 = document.getElementById(cardIds[1]);
+                    card1.style.background = 'radial-gradient(lightblue, #007bff)';
+                    card1.style.backgroundSize = "cover";
+                    card1.innerHTML = "";
+                    card2.style.background = 'radial-gradient(lightblue, #007bff)';
+                    card2.style.backgroundSize = "cover";
+                    card2.innerHTML = "";
+                    cardValues = []; // Clear array
+                    cardIds = []; // Clear array
+                }
+                setTimeout(flipOver, 1000);
+            }
+        }
+    }
+}
+
+function turnCounting() {
+    //Counting the turns
+    var turnsTakenString; //variable
+    turnsTaken++; //Add 1 to the variable
+    turnsTaken.toString(); //Convert from number to string
+    turnsTakenString = "Turns Taken: " + turnsTaken; //Puts the string together
+    document.getElementById("turnCounter").innerHTML = turnsTakenString; //Updates the html in real time
+}
+
+function playerNameInput() {
+    exampleName = "Ready Player One";
+    var person = prompt("Please enter your name:", exampleName);
+    if (person == null || person == "" || person == exampleName) {
+        playerName = "Guest";
+    } else {
+        playerName = person;
+    }
+    //document.getElementById("playerName").innerHTML = playerName; //Puts the name into the HTML ID Tag playerName
+}
+
+function results() {
+    //Reorder leaderboard
+    leaderArray.unshift([turnsTaken, playerName]);
+    let sortedArray = leaderArray.sort((a, b) => {
+        if (a > b) return 1;
+        else if (b > a) return -1;
+        else return 0;
+    });
+
+
+    debugger;
+    var updateLeaderboard = "";
+    generateLeaderboard();
+    i = 0;
+    document.getElementById("score_area").innerHTML = updateLeaderboard;
+}
+
+function guessingString(length) {
+    var guessingString = ''
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++)
+        guessingString += possible.charAt(Math.floor(Math.random() * possible.length));
+    generatedGuessString = guessingString;
+    return guessingString;
+}
+
+function guessingTime() {
+    var usersGuess = document.getElementById("userGuessBox").value; //Reads text box with users input in
+    var guessStringLeft;
+    if (guessesLeft > 0) {
+        if (generatedGuessString === usersGuess) {
+            playerNameInput(); // Take players name
+            results(); //Leaderboard functions
+        } else {
+            guessesLeft--
+            if (guessesLeft > 1) { guessStringLeft = guessesLeft + " Guesses Left" } else { guessStringLeft = "Last Guess" }
+            turnCounting();
+            document.getElementById("guessButton").innerText = guessStringLeft;
+        }
+    }
+    if (guessStringLeft == 0) {
+        alert("You have no more guesses left!");
+        hideGuess();
+    };
+
+}
 
 function generateLeaderboard() {
     updateLeaderboard += "<table class=\"table table-borderless\">";
@@ -135,141 +274,15 @@ function generateLeaderboard() {
     updateLeaderboard += "                                <\/tr>";
     updateLeaderboard += "                            <\/tbody>";
     updateLeaderboard += "                        <\/table>";
+    document.getElementById("score_area").innerHTML = updateLeaderboard;
 };
 
-function newGame() {
+function hideGuess() {
+    document.getElementById("userGuessBox").hidden = true;
+    document.getElementById("guessButton").hidden = true;
+}
 
-    cardFlipped = 0; //Reset Value
-    turnsTaken = 0; //Reset Value
-    guessesLeft = 3; //Reset Value
-    //Calls the shuffle command and adds Divs to the game_area
-    var out = '';
-    cardArray.cardShuffle();
-    for (var i = 0; i < cardArray.length; i++) {
-        out += '<div id="card_' + i + '" onclick="cardFlip(this,\'' + cardArray[i] + '\')"></div>';
-    }
-    out += '<span>' + guessingString(10) + '</span>';
-    turnsTakenString = ""; //Hides the text so that it doesnt look unsightly
-    document.getElementById("turnCounter").innerHTML = turnsTakenString;
-    document.getElementById('game_area').innerHTML = out;
-    document.getElementById("guessButton").innerText = "3 Guesses Left";
+function showGuess() {
     document.getElementById("userGuessBox").hidden = false;
     document.getElementById("guessButton").hidden = false;
-};
-
-function cardFlip(card, val) {
-    //Flipping Cards
-    if (card.innerHTML == "" && cardValues.length < 2) { //If the cards match
-        card.style.background = '#FFF';
-        card.style.backgroundImage = 'radial-gradient(lightblue, cornflowerblue)'; //Displays a circle of light blue, behind the text
-        card.innerHTML = val;
-        if (cardValues.length == 0) {
-            turnCounting();
-            cardValues.push(val); //Push the Value into the array
-            cardIds.push(card.id); //Push the Card Id into the array
-        } else if (cardValues.length == 1) {
-            cardValues.push(val); //Clicking on the second card and pushing the Value into the Array
-            cardIds.push(card.id); //Pushing the second Card ID
-            if (cardValues[0] == cardValues[1]) {
-                cardFlipped += 2;
-                var card1 = document.getElementById(cardIds[0]);
-                var card2 = document.getElementById(cardIds[1]);
-                card1.style.visibility = 'hidden'; //Once match is made hide the cards
-                card2.style.visibility = 'hidden'; //Once match is made hide the cards
-                cardValues = []; // Clear array
-                cardIds = []; // Clear array
-                // Check to see if the whole board is cleared
-                if (cardFlipped == cardArray.length) { //If all the cards have been flipped
-                    //alert("Game Completed, you have taken " + turnsTaken + " turns to complete the game.  Lets see where you have come on the Leader Board");
-                    document.getElementById('game_area').innerHTML = "Well Done";
-                    playerNameInput(); // Take players name
-                    results(); //Leaderboard functions
-                }
-            } else { //This will run if there are no matches
-                function flipOver() {
-                    // Flip the 2 tiles back over
-                    var card1 = document.getElementById(cardIds[0]);
-                    var card2 = document.getElementById(cardIds[1]);
-                    card1.style.background = 'radial-gradient(lightblue, #007bff)';
-                    card1.style.backgroundSize = "cover";
-                    card1.innerHTML = "";
-                    card2.style.background = 'radial-gradient(lightblue, #007bff)';
-                    card2.style.backgroundSize = "cover";
-                    card2.innerHTML = "";
-                    cardValues = []; // Clear array
-                    cardIds = []; // Clear array
-                }
-                setTimeout(flipOver, 1000);
-            }
-        }
-    }
-}
-
-function turnCounting() {
-    //Counting the turns
-    var turnsTakenString; //variable
-    turnsTaken++; //Add 1 to the variable
-    turnsTaken.toString(); //Convert from number to string
-    turnsTakenString = "Turns Taken: " + turnsTaken; //Puts the string together
-    document.getElementById("turnCounter").innerHTML = turnsTakenString; //Updates the html in real time
-}
-
-function playerNameInput() {
-    exampleName = "Ready Player One";
-    var person = prompt("Please enter your name:", exampleName);
-    if (person == null || person == "" || person == exampleName) {
-        playerName = "Guest";
-    } else {
-        playerName = person;
-    }
-    //document.getElementById("playerName").innerHTML = playerName; //Puts the name into the HTML ID Tag playerName
-}
-
-function results() {
-    //Reorder leaderboard
-    leaderArray.unshift([turnsTaken, playerName]);
-    let sortedArray = leaderArray.sort((a, b) => {
-        if (a > b) return 1;
-        else if (b > a) return -1;
-        else return 0;
-    });
-
-
-
-    var updateLeaderboard = "";
-    generateLeaderboard();
-    i = 0;
-    document.getElementById("score_area").innerHTML = updateLeaderboard;
-}
-
-function guessingString(length) {
-    var guessingString = ''
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < length; i++)
-        guessingString += possible.charAt(Math.floor(Math.random() * possible.length));
-    generatedGuessString = guessingString;
-    return guessingString;
-}
-
-function guessingTime() {
-    var usersGuess = document.getElementById("userGuessBox").value; //Reads text box with users input in
-    var guessStringLeft;
-    if (guessesLeft > 0) {
-        if (generatedGuessString === usersGuess) {
-            playerNameInput(); // Take players name
-            results(); //Leaderboard functions
-        } else {
-            guessesLeft--
-            if (guessesLeft > 1) { guessStringLeft = guessesLeft + " Guesses Left" } else { guessStringLeft = "Last Guess" }
-            turnCounting();
-            document.getElementById("guessButton").innerText = guessStringLeft;
-        }
-    }
-    if (guessStringLeft == 0) {
-        alert("You have no more guesses left!");
-        document.getElementById("userGuessBox").hidden = true;
-        document.getElementById("guessButton").hidden = true;
-    };
-
 }

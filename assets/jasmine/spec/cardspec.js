296fb9b66ac038f09e6738183edbc0116fd6b12c
Stephen Jackson <jjackson19862017@gmail.com>
2019-12-16 10:04:47 +0000

    Jasmine Testing Working :)

--- assets/jasmine/spec/cardspec.js
+++ assets/jasmine/spec/cardspec.js
@@ -1,13 +1,41 @@
-describe("Calculator", function() {
+/*describe("Calculator", function() {
+    beforeEach(function() {
+        calc = new Calculator;
+    });
     describe("Addition Testing", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            expect(addition(19, 7)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
         it("should return an error if we don't supply two numbers", function() {
-            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
+});*/
+
+describe('Player Name Input', function() {
+    it('null should return Guest',
+        function() {
+            expect(playerNameInput("")).toBe("Guest");
+        });
+    it('"" should return Guest',
+        function() {
+            expect(playerNameInput("")).toBe("Guest");
+        });
+    it('exampleName should return Guest',
+        function() {
+            expect(playerNameInput("Ready Player One")).toBe("Guest");
+        });
+    it("Stephen should return Stephen",
+        function() {
+            expect(playerNameInput("Stephen")).toBe("Stephen");
+        });
 });
